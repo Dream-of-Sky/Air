7ef73719228f9d133929fbc4bde3696e815629c5
Sweet-dream <992308987@qq.com>
2016-09-04 16:24:10 +0800

    This is just a update！！！！

---
+++ app/src/main/java/com/example/team/four/airlines/y_jiangpeng/P_TesstActivity.java
@@ -0,0 +1,17 @@
+package com.example.team.four.airlines.y_jiangpeng;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.team.four.airlines.R;
+
+public class P_TesstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p__tesst);
+        System.out.println("鹏activity请以此activity 名字格式命名创建的Activity");
+
+    }
+}
---
+++ app/src/main/java/com/example/team/four/airlines/z_tianjiao/J_TestActivity.java
@@ -0,0 +1,16 @@
+package com.example.team.four.airlines.z_tianjiao;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.team.four.airlines.R;
+
+public class J_TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_j__test);
+        System.out.println("张天娇的activity请以此activity 名字格式命名创建的Activity");
+    }
+}
