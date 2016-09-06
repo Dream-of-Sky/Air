47b945ccc0153373d0568fd508bbb923257ea24a
Sweet-dream <992308987@qq.com>
2016-09-06 21:03:10 +0800

    test!!!

---
+++ app/src/main/java/com/example/team/four/airlines/h_jingbo/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.example.team.four.airlines.h_jingbo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.team.four.airlines.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
