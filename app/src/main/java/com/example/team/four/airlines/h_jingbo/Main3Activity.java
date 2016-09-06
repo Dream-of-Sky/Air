9a350f12ac9ed4f256bb36ed439323f0903a9c8c
Sweet-dream <992308987@qq.com>
2016-09-06 20:37:45 +0800

    test

---
+++ app/src/main/java/com/example/team/four/airlines/h_jingbo/Main3Activity.java
@@ -0,0 +1,16 @@
+package com.example.team.four.airlines.h_jingbo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.team.four.airlines.R;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        System.out.println("醉了");
+    }
+}
